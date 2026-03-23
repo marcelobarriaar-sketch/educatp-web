8c48cfa4431ba7e64a0e37b961f7d3b2678d5cc2
MARCELOBARRIAPELAO <marcelo.barriaar@gmail.com>
2026-03-23 22:17:52 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,30 +7,38 @@ import SpecialtyDetail from './pages/SpecialtyDetail';
 import Blog from './pages/Blog';
 import Internships from './pages/Internships';
 import Playground from './pages/Playground';
-import Admin from './pages/Admin'; // 👈 agregado
+import Admin from './pages/Admin';
+
+function AppContent() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
+  return (
+    <div className="min-h-screen flex flex-col font-sans text-slate-900">
+      {!isAdminRoute && <Navbar />}
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/especialidades" element={<Specialties />} />
+          <Route path="/especialidades/:id" element={<SpecialtyDetail />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/practicas" element={<Internships />} />
+          <Route path="/juegos" element={<Playground />} />
+          <Route path="/recursos" element={<Specialties />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
+
+      {!isAdminRoute && <Footer />}
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col font-sans text-slate-900">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/especialidades" element={<Specialties />} />
-            <Route path="/especialidades/:id" element={<SpecialtyDetail />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/practicas" element={<Internships />} />
-            <Route path="/juegos" element={<Playground />} />
-            <Route path="/recursos" element={<Specialties />} />
-            
-            {/* 👇 NUEVA RUTA ADMIN */}
-            <Route path="/admin" element={<Admin />} />
-
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
