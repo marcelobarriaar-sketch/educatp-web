609fc8f470a1a7931c41809853daa35209f94c79
MARCELOBARRIAPELAO <marcelo.barriaar@gmail.com>
2026-04-09 16:50:00 -0400

    Create resourseOptions.ts

---
+++ src/data/resourseOptions.ts
@@ -0,0 +1,34 @@
+export const resourceCategories = [
+  'Administración',
+  'Informática',
+  'Emprendimiento',
+  'Legislación laboral',
+  'Normativa comercial y tributaria',
+  'Desarrollo y bienestar organizacional',
+  'Organización de oficinas',
+  'Procesos administrativos',
+  'Orientación',
+  'Convivencia escolar',
+  'General',
+];
+
+export const resourceTypes = [
+  'Documento',
+  'PDF',
+  'Presentación',
+  'Video',
+  'Guía',
+  'Formulario',
+  'Plantilla',
+  'Enlace externo',
+  'Otro',
+];
+
+export const educationLevels = [
+  '1° Medio',
+  '2° Medio',
+  '3° Medio TP',
+  '4° Medio TP',
+  'Docentes',
+  'General',
+];
